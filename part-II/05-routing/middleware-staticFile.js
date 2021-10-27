a5f7732d9460f2104115f0f71febb6ea1c92ea91
Gonzalo FU <GFU-code@protonmail.ch>
2021-10-26 21:05:51 -0300

    Part II, capitulo 05: Extendiendo static file - Manejando múltiples enrutamientos de archivos estáticos

--- part-II/05-routing/middleware-staticFile.js
+++ part-II/05-routing/middleware-staticFile.js
@@ -3,11 +3,23 @@ const path = require("path");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, "static");
-app.use(express.static(publicPath));
-
 // Extendiendo la manera en como se pueden abordar los
 // middleware para servir archivos estáticos.
+
+const publicPath = path.resolve(__dirname, "static");
+app.use(express.static(publicPath));
+// Tratando con múltiples enrutamientos a directorios públicos
+const userUploadsPath = path.resolve(__dirname, "uploads");
+app.use(express.static(userUploadsPath));
+
+/* Si queremos diferenciar sin ambiguedad cuando acceder a un recurso que puede
+estar en ambos directorios estáticos, siempre podemos especificar la ruta desde la 
+cuál se llamará el directorio. Por ejemplo:
+
+app.use("/public", express.static("publicPath"));
+app.use("/uploads", express.static("userUploadsPath"));
+
+*/
 
 // Middleware para servir archivos estáticos con una ruta especifica.
 const photoPath = path.resolve(__dirname, "offensive-photos-folder");
