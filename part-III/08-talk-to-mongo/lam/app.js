715f58f6d84eeabf2698a2a291a5805196d6e724
Gonzalo FU <GFU-code@protonmail.ch>
2021-11-05 16:39:25 -0300

    Part III, capitulo 08: Setting up Passport, step 1/3 - Configuración del middleware passport

--- part-III/08-talk-to-mongo/lam/app.js
+++ part-III/08-talk-to-mongo/lam/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
 
 // Importaciónes
 const routes = require("./routes");
@@ -21,16 +22,31 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 
 // Middlewares
+// body-parser nos permite parsear los formularios HTML
 app.use(bodyParser.urlencoded({ extended: false }));
+// cookie-parser se encarga de parsear las cookies desde el navegador, y es requerido para
+// administrar la sesión de los usuarios.
 app.use(cookieParser());
+// express-session nos permite almacenar las sesiones de los usuarios en diferentes navegadores
+// webs.
 app.use(
   session({
+    // secret es un conjunto de caracteres random que usamos para encriptar
+    // la sesión desde cliente.
     secret: "AcavaNuestrAPalabraSecretaParaENcripTar",
+    // resave es requerido. Cuando tiene un valor de true la sesión se actualiza incluso
+    // si esta no se ha modificado.
     resave: true,
+    // saveUninitialized es requerido. Restablece las sesiones que no estan inicializadas.
     saveUninitialized: true,
   })
 );
+// flash facilita la comunicación de mensajes de error.
 app.use(flash());
+// Inicializa el módulo de passport.
+app.use(passport.initialize());
+// Maneja las sesiones de passport.
+app.use(passport.session());
 app.use(routes);
 
 // Inciamos el servidor para escuchar en el puerto definido
