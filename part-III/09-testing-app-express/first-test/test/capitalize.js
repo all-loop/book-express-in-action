9ac28214bb5ea7c6a32a8e2e40d0d72824ebe88e
Gonzalo FU <GFU-code@protonmail.ch>
2021-11-09 11:49:30 -0300

    Part III, capitulo 09: testing capitalize - aumentando la robustez de las pruebas

--- part-III/09-testing-app-express/first-test/test/capitalize.js
+++ part-III/09-testing-app-express/first-test/test/capitalize.js
@@ -20,4 +20,20 @@ describe("capitalize", function () {
   it("leaves empty strings alone", function () {
     expect(capitalize("")).to.equal("");
   });
+
+  it("leaves strings with no words alone", function () {
+    expect(capitalize("   ")).to.equal("   ");
+    expect(capitalize("123")).to.equal("123");
+  });
+
+  it("capitalizes multiple-word strings", function () {
+    expect(capitalize("what is Express?")).to.equal("What is express?");
+    expect(capitalize("i love lamp")).to.equal("I love lamp");
+  });
+
+  it("leaves already-capitalized words alone", function () {
+    expect(capitalize("Express")).to.equal("Express");
+    expect(capitalize("Evan")).to.equal("Evan");
+    expect(capitalize("Catman")).to.equal("Catman");
+  });
 });
