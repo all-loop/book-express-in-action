45051d9f36204e4588b05f93bbe263a3e548fc02
Gonzalo FU <GFU-code@protonmail.ch>
2021-11-13 12:56:02 -0300

    Part III, capitulo 09: user agent app - pruebas de HTML y Texto pasadas

--- part-III/09-testing-app-express/user-agent/app.js
+++ part-III/09-testing-app-express/user-agent/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
 
+// Restructuramos la solicitud GET para acomodarnos a las pruebas
+// de texto y HTML.
 app.get("/", (req, res) => {
-  res.type("text");
-  res.send(req.headers["user-agent"]);
+  const userAgent = req.headers["user-agent"] || "none";
+  if (req.accepts("html")) {
+    res.render("index", { userAgent });
+  } else {
+    res.type("text");
+    res.send(userAgent);
+  }
 });
 
 app.listen(app.get("port"), () => {
