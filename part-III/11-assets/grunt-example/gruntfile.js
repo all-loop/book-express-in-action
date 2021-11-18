1852602dea053007cc5a96617e3d918ab11c3ac5
Gonzalo FU <GFU-code@protonmail.ch>
2021-11-18 11:07:28 -0300

    Part III, capitulo 11: grunt less - automatizando la compilación y minificación de un archivo LESS con Grunt

--- part-III/11-assets/grunt-example/gruntfile.js
+++ part-III/11-assets/grunt-example/gruntfile.js
@@ -1,5 +1,21 @@
 module.exports = function (grunt) {
-  grunt.registerTask("default", "Say Hello World.", function () {
-    grunt.log.write("Hello World!");
+  // configura los ajustes para cada tarea de grunt
+  grunt.initConfig({
+    less: {
+      main: {
+        options: {
+          paths: ["css"],
+        },
+        files: {
+          "tmp/build/main.css": "css/main.less",
+        },
+      },
+    },
   });
+
+  // Carga el plugin de LESS para Grunt
+  grunt.loadNpmTasks("grunt-contrib-less");
+
+  // Define la tarea de compilación para LESS
+  grunt.registerTask("default", ["less"]);
 };
